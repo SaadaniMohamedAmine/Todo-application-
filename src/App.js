107e4881b9e2c0e6f44752d3174114d056fdcda4
Saadani Mohmaed Amine <saadanimohamedamine@gmail.com>
2021-01-02 10:55:07 +0100

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Card, Button } from "react-bootstrap";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Profile">
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
